feedf73528213e070e268afa15c53beb41d5d2c0
gnara0719 <ppjw254236@gmail.com>
2026-02-10 11:28:19 +0900

    feat: RefreshTokenRequest DTO를 record로 변환
    - 기존 클래스 형태에서 record로 변경
    - @NotBlank 및 @JsonProperty 어노테이션 유지

--- src/main/java/com/codeit/jwtpractice/dto/auth/RefreshTokenRequest.java
+++ src/main/java/com/codeit/jwtpractice/dto/auth/RefreshTokenRequest.java
@@ -3,9 +3,9 @@ package com.codeit.jwtpractice.dto.auth;
 import com.fasterxml.jackson.annotation.JsonProperty;
 import jakarta.validation.constraints.NotBlank;
 
-public class RefreshTokenRequest {
-
-    @NotBlank(message = "Refresh Token은 필수")
-    @JsonProperty("refresh_token")
-    String refreshToken;
+public record RefreshTokenRequest(
+        @NotBlank(message = "Refresh Token은 필수")
+        @JsonProperty("refresh_token")
+        String refreshToken
+) {
 }
